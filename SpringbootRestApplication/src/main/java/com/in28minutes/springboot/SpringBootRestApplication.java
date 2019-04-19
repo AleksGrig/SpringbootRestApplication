01063ec2e7b908eed6b7c417c58cc56db4696e3e
Aleksey Grigoruk <ullitap@gmail.com>
2019-04-19 13:16:57 +0300

    First commit.

---
+++ SpringbootRestApplication/src/main/java/com/in28minutes/springboot/SpringBootRestApplication.java
@@ -0,0 +1,18 @@
+package com.in28minutes.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan("/com/in28minutes") // ComponentScan automatically searches in the same package
+public class SpringBootRestApplication {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		ApplicationContext ctx = SpringApplication.run(SpringBootRestApplication.class, args);
+
+	}
+
+}
---
+++ SpringbootRestApplication/src/main/java/com/in28minutes/springboot/service/WelcomeService.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.service;
+
+import org.springframework.stereotype.Service;
+
+//Spring to manage this bean and create instance of this class
+//@Component
+@Service
+public class WelcomeService {
+
+	public String retrieveWelcomeMessage() {
+		return "Good morning! Updated. Spring-boot-devtools added. Now it works!";
+	}
+}
