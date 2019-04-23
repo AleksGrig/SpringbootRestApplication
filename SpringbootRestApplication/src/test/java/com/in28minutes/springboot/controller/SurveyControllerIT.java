bdce0e1ec2e102296d3526ea474ab05b6ed5e8d1
Aleksey Grigoruk <ullitap@gmail.com>
2019-04-23 10:21:27 +0300

    User and admin added, integration and unit test fixed while using spring security

--- SpringbootRestApplication/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
+++ SpringbootRestApplication/src/test/java/com/in28minutes/springboot/controller/SurveyControllerIT.java
@@ -2,6 +2,7 @@ package com.in28minutes.springboot.controller;
 
 import static org.junit.Assert.assertTrue;
 
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.List;
 
@@ -19,11 +20,13 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.codec.Base64;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.in28minutes.springboot.SpringBootRestApplication;
 import com.in28minutes.springboot.model.Question;
 
+@SuppressWarnings("deprecation")
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringBootRestApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SurveyControllerIT {
@@ -41,8 +44,17 @@ public class SurveyControllerIT {
 		return "http://localhost:" + port + adress;
 	}
 
+	private String createHttpAuthenticationHeaderValue(String userID, String password) {
+		// userID, password, basic authentication
+		String auth = userID + ":" + password;
+		byte[] encodedAuth = Base64.encode(auth.getBytes(Charset.forName("US-ASCII")));
+		String authHeader = "Basic " + new String(encodedAuth);
+		return authHeader;
+	}
+
 	@Before
 	public void before() {
+		headers.add("Authorization", createHttpAuthenticationHeaderValue("user", "password"));
 		headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));// Accept - application/json
 	}
 
